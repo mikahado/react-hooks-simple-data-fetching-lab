8f3d3038d39c18ccaf024d1d2e591bc954ca0c00
Michael <mikahado@gmail.com>
2022-11-02 12:59:30 -0400

    simple data fetch lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+    const [randoDog, setRandoDog] = useState(null)
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(imageDog => setRandoDog(imageDog.message))
+    }, []);
+
+    if (!randoDog) return <h1> . . . Loading . . . </h1>
+    
+    return (
+        <img src={randoDog} alt="A Random Dog"/>
+    )  
+}
+
+export default App
